feadb9e19fc8e03617c669e094ca3ea1c5607f48
kkang2 <mallakkang@gmail.com>
2020-09-08 11:44:55 +0900

    Add files via upload
    boot-pattern project first commit!

---
+++ boot-pattern/src/main/java/com/boot/pattern/BootPatternApplication.java
@@ -0,0 +1,13 @@
+package com.boot.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootPatternApplication.class, args);
+    }
+
+}
