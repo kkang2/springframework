10f2cc5deecc319ac74eb13af2c71bc4ba4888cf
kkang2 <mallakkang@gmail.com>
2020-09-10 20:01:38 +0900

    Add files via upload
    boot web pattern 프로젝트 첫 커밋!

---
+++ boot-web-pattern/src/main/java/com/boot/pattern/BootWebPatternApplication.java
@@ -0,0 +1,13 @@
+package com.boot.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWebPatternApplication.class, args);
+    }
+
+}
