f6afe1c848f60180b9cb4ec3e13c3424574d52b9
kkang2 <mallakkang@gmail.com>
2020-09-15 13:20:21 +0900

    Add files via upload
    mybatis 샘플 첫 커밋!

---
+++ boot-mybatis-pattern/src/main/java/com/boot/pattern/BootMybatisPatternApplication.java
@@ -0,0 +1,13 @@
+package com.boot.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisPatternApplication.class, args);
+    }
+
+}
---
+++ boot-mybatis-pattern/src/test/java/com/boot/pattern/BootMybatisPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMybatisPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
