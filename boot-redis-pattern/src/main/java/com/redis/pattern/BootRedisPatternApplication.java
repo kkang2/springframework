5bc3e93d4264c55c9f7a2b0ed95a30e7be7eb854
kkang2 <mallakkang@gmail.com>
2024-08-16 19:41:53 +0900

    redis 샘플 프로젝트 첫 커밋

---
+++ boot-redis-pattern/src/main/java/com/redis/pattern/BootRedisPatternApplication.java
@@ -0,0 +1,13 @@
+package com.redis.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedisPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRedisPatternApplication.class, args);
+	}
+
+}
---
+++ boot-redis-pattern/src/test/java/com/redis/pattern/BootRedisPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRedisPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
