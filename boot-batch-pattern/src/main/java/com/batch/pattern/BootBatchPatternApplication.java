cc9335c10c5925b049f341e4abb35246862873e1
kkang2 <mallakkang@gmail.com>
2024-08-18 21:31:20 +0900

    batch 샘플 프로젝트 첫 푸쉬

---
+++ boot-batch-pattern/src/main/java/com/batch/pattern/BootBatchPatternApplication.java
@@ -0,0 +1,13 @@
+package com.batch.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBatchPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBatchPatternApplication.class, args);
+	}
+
+}
---
+++ boot-batch-pattern/src/test/java/com/batch/pattern/BootBatchPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBatchPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
