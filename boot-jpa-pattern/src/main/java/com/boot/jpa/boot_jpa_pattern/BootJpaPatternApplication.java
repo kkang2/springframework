48aafe9212cc6b2bcca67cbe86a46f61ee180d0c
kkang2 <mallakkang@gmail.com>
2024-09-16 20:07:49 +0900

    jpa 커밋

---
+++ boot-jpa-pattern/src/main/java/com/boot/jpa/boot_jpa_pattern/BootJpaPatternApplication.java
@@ -0,0 +1,13 @@
+package com.boot.jpa.boot_jpa_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaPatternApplication.class, args);
+	}
+
+}
---
+++ boot-jpa-pattern/src/test/java/com/boot/jpa/boot_jpa_pattern/BootJpaPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.jpa.boot_jpa_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
